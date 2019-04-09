6faa26eebce794839f2daa1af13605cf4273f103
wk-j <somnuk.wk@gmail.com>
2019-04-09 13:08:51 +0700

    Initial commit

---
+++ src/Run/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Run {
+
+    class NpmSettings : ToolSettings {
+
+    }
+
+    class NpmTool : Tool<NpmSettings> {
+        protected override IEnumerable<string> GetToolExecutableNames() {
+            return new[] { "npm" };
+        }
+
+        protected override string GetToolName() {
+            return "npm";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+
+        }
+    }
+}
